183732735e7415c20dc214c4064bea20843bd89d
Nirmal Tamang <lamanirmal27@gmail.com>
2025-04-26 21:53:05 +0545

    copy feature added

--- project2/src/component/Editor.tsx
+++ project2/src/component/Editor.tsx
@@ -1,6 +1,6 @@
-
+import { FaRegCopy, FaCheck } from "react-icons/fa6";
 import FullScreenButton from "./FullScreenButton";
-
+import { useRef, useState } from "react";
 interface EditorProps {
     markdown: string;
     setMarkdown: (value: string) => void;
@@ -8,16 +8,45 @@ interface EditorProps {
     setFullScreen: (view: "editor" | "preview" | "none") => void
 }
 const Editor : React.FC<EditorProps> = ({markdown, setMarkdown, fullScreen, setFullScreen}) => {
-
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [copied, setCopied] = useState(false);
   if (fullScreen == "preview") return null;
+
+
+  const handleCopy = () => {
+    if (textareaRef.current) {
+      navigator.clipboard.writeText(textareaRef.current.value)
+        .then(() => {
+          console.log('Copied to clipboard!');
+          setCopied(true);
+        })
+        .catch((err) => {
+          console.error('Failed to copy: ', err);
+          setCopied(false)
+        });
+    }
+  };
   return (
 
   <div className={`flex flex-col w-full ${fullScreen ==='editor' ? 'h-screen' : 'h-[200px]'} pt-7`}>
     <div className="flex justify-between items-center">
-      <h2 className="text-xl font-bold">Editor</h2>
-      <FullScreenButton target='editor' fullScreen={fullScreen} setFullScreen={setFullScreen}  />
+      <h2 className="text-xl font-bold text-gray-800">Editor</h2>
+      <div className="flex items-center space-x-4">
+      <button 
+        className="cursor-pointer text-gray-600 transition-colors" 
+        onClick={handleCopy} 
+      >
+        {!copied ? <FaRegCopy /> : <FaCheck />}
+      </button>
+      <FullScreenButton 
+        target="editor" 
+        fullScreen={fullScreen} 
+        setFullScreen={setFullScreen} 
+      />
+      </div>
     </div>
       <textarea
+      ref={textareaRef}
       id="editor"
       value={markdown}
       onChange={(e) => setMarkdown(e.target.value)}
